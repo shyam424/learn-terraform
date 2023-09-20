edb54c05133d156fe036715bb13f9e14f5b7e1c9
Shyam <shyamneo24@gmail.com>
2023-09-20 15:39:16 +0530

    terraform-server

--- 02-ec2/server.tf
+++ 02-ec2/server.tf
@@ -1,17 +1,8 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.17"
-    }
-  }
-}
-
 provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "test" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
 
