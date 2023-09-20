b50060785651cebd64833d2a22121d023530d3d2
Shyam <shyamneo24@gmail.com>
2023-09-20 13:04:13 +0530

    terraform-server

--- 02-ec2/server.tf
+++ 02-ec2/server.tf
@@ -1,4 +1,13 @@
+provider "aws"{
+  profile = "default"
+  region  = "us-east-1"
+}
+
 resource "aws_instance" "web" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
