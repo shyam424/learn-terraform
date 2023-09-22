4b2720c0b8f8ed61b72bc0efbcab87bfec24a48b
Shyam <shyamneo24@gmail.com>
2023-09-22 12:38:18 +0530

    terraform-functions

---
+++ 04-functions/sample.tf
@@ -0,0 +1,9 @@
+variable "fruits" {
+  default = [ "apple", "banana"]
+}
+
+output "test_functions" {
+  # value = var.fruits[2]  this one will throw error because we have only two values in the list and list start from 0 which is 0,1 and there is no 2
+value = element(var.fruits,2)
+  # element function will make sure that it returns zero if the condition is not satisfied
+}
