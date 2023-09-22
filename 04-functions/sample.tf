21406b66b984b3f0f76c71f8261a04b69de6cced
Shyam <shyamneo24@gmail.com>
2023-09-22 12:47:24 +0530

    terraform-functions

--- 04-functions/sample.tf
+++ 04-functions/sample.tf
@@ -6,5 +6,5 @@ output "test_functions" {
   # value = var.fruits[2]  this one will throw error because we have only two values in the list and list start from 0 which is 0,1 and there is no 2
 value = element(var.fruits,2)
   # element function will make sure that it wont throw the error which CYCLES the vaules
-  #TRY function will return 0 if the value is not there
+  #TRY function will return 0 if the value is not DECLARED
 }
