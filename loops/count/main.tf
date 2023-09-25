f760675e272a0406bca208ceccf37d5561aa5d92
Shyam <shyamneo24@gmail.com>
2023-09-25 11:52:40 +0530

    terraform-loops

--- loops/count/main.tf
+++ loops/count/main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 variable "components"{
   default = ["frontend","mongodb"]
 }
@@ -5,6 +9,8 @@ variable "components"{
 resource "aws_instance" "rabbitmq" {
 
   count = length(var.components)
+  #//Count= will let you know how many time sit should run in loops
+  #Count is a keyword
 
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.small"
