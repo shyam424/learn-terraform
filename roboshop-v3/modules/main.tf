d7ebbb6322c2b880d155baaf47a5c3457028bfcc
Shyam <shyamneo24@gmail.com>
2023-09-25 19:38:03 +0530

    terraform-modules

--- roboshop-v3/modules/main.tf
+++ roboshop-v3/modules/main.tf
@@ -30,7 +30,7 @@ cd /home/centos/roboshop-ansible
 git pull
 sleep 30
 ansible-playbook -i ${var.name}.devopspractice23.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e
-component = ${var.name}
+components = ${var.name}
 EOF
 
   }
